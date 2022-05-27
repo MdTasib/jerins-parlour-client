c21b13d656e323c9ed594151055e9f46f95b71ab
MdTasib <ohidulalam442@gmail.com>
2022-05-27 16:26:55 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#F63E7B",
+					secondary: "#FFF8F5",
+					accent: "#F4F7FC",
+					neutral: "#3d4451",
+					"base-100": "#ffffff",
+				},
+			},
+		],
+	},
+	plugins: [require("daisyui")],
+};
